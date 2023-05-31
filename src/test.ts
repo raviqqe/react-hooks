739ce39a0239e5cfb3b7dc533d9887677ea54d26
Yota Toyama <raviqqe@gmail.com>
2023-05-31 15:24:34 +1000

    Fix

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,3 @@
 import { vi } from "vitest";
 
-(global as unknown as { jest: unknown }).jest = {
-  advanceTimersByTime: (time: number) => vi.advanceTimersByTime(time),
-};
+(global as unknown as { jest: unknown }).jest = vi;
