63c7559076d0b89a17910a93f8f7328492c2a61b
Yota Toyama <raviqqe@gmail.com>
2023-05-31 14:23:37 +1000

    Fix

--- src/debounce.ts
+++ src/debounce.ts
@@ -1,9 +1,20 @@
-import { type DependencyList } from "react";
+import { useRef, type DependencyList, useEffect } from "react";
 
 export const useDebounce = (
   callback: () => void,
   delay: number,
   dependencies: DependencyList
-) => {
-  foo;
+): void => {
+  const timer = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const callbackRef = useRef<() => void>(callback);
+
+  useEffect(() => {
+    callbackRef.current = callback;
+
+    return () => {
+      if (timer.current) {
+        clearTimeout(timer.current);
+      }
+    };
+  }, dependencies);
 };
