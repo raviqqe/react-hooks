81dff1e44be32e53cfe26878663e6fcc4b3525d7
Yota Toyama <raviqqe@gmail.com>
2023-05-31 15:31:27 +1000

    Initialize package (#2)

---
+++ src/debounce.ts
@@ -0,0 +1,29 @@
+import { useRef, type DependencyList, useEffect, useCallback } from "react";
+
+export const useDebounce = (
+  callback: () => void,
+  delay: number,
+  dependencies: DependencyList
+): void => {
+  const timer = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const callbackRef = useRef<() => void>(callback);
+
+  const clear = useCallback(() => {
+    if (timer.current) {
+      clearTimeout(timer.current);
+    }
+  }, []);
+
+  useEffect(() => {
+    callbackRef.current = callback;
+
+    clear();
+    timer.current = setTimeout(() => {
+      callbackRef.current();
+      timer.current = null;
+    }, delay);
+
+    return clear;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, dependencies);
+};
---
+++ src/test.ts
@@ -0,0 +1,3 @@
+import { vi } from "vitest";
+
+(global as unknown as { jest: unknown }).jest = vi;
