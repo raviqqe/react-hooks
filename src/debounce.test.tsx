5fbd6017459e5361a9bb71ca1b8e95e9c3e73a3f
Yota Toyama <raviqqe@gmail.com>
2023-05-31 14:56:12 +1000

    Fix

--- src/debounce.test.tsx
+++ src/debounce.test.tsx
@@ -1,6 +1,6 @@
+import { act, render, waitFor } from "@testing-library/react";
 import { expect, it } from "vitest";
 import { useDebounce } from "./debounce.js";
-import { act, render, waitFor } from "@testing-library/react";
 
 interface Props {
   value: number;
@@ -14,44 +14,91 @@ const Component = ({ value, onChange }: Props) => {
 };
 
 it("runs without any error", () => {
-  render(<Component value={1} onChange={() => {}} />);
+  render(<Component onChange={() => {}} value={1} />);
 });
 
-it("runs a callback", () => {
+it("runs a callback", async () => {
   let index = 0;
 
-  act(() => {
+  await act(() =>
     render(
       <Component
-        value={1}
         onChange={() => {
           index++;
         }}
+        value={1}
       />
-    );
-  });
+    )
+  );
 
-  waitFor(() => expect(index).toBe(1));
+  await waitFor(() => expect(index).toBe(1));
 });
 
-it("runs a callback twice", () => {
+it("runs a callback twice", async () => {
   let index = 0;
 
   const renderComponent = (value: number) =>
     render(
       <Component
-        value={value}
         onChange={() => {
           index++;
         }}
+        value={value}
       />
     );
 
-  act(() => renderComponent(1));
+  await act(() => renderComponent(1));
 
-  waitFor(() => expect(index).toBe(1));
+  await waitFor(() => expect(index).toBe(1));
 
-  act(() => renderComponent(2));
+  await act(() => renderComponent(2));
 
-  waitFor(() => expect(index).toBe(2));
+  await waitFor(() => expect(index).toBe(2));
+});
+
+it("debounces two calls into one", async () => {
+  let index = 0;
+
+  const renderComponent = (value: number) =>
+    render(
+      <Component
+        onChange={() => {
+          index++;
+        }}
+        value={value}
+      />
+    );
+
+  await act(() => renderComponent(1));
+  await act(() => renderComponent(2));
+
+  await waitFor(() => expect(index).toBe(1));
+
+  await act(() => renderComponent(3));
+
+  await waitFor(() => expect(index).toBe(2));
+});
+
+it("debounces three calls into one", async () => {
+  let index = 0;
+
+  const renderComponent = (value: number) =>
+    render(
+      <Component
+        onChange={() => {
+          index++;
+        }}
+        value={value}
+      />
+    );
+
+  await act(() => renderComponent(1));
+  await act(() => renderComponent(2));
+  await act(() => renderComponent(3));
+
+  await waitFor(() => expect(index).toBe(1));
+
+  await act(() => renderComponent(3));
+
+  await waitFor(() => expect(index).toBe(2));
 });
