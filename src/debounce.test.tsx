5d74b06d51ac11c2bbf66770e4359b84a3cc03f4
Yota Toyama <raviqqe@gmail.com>
2024-08-08 19:36:38 +0900

    Fix

--- src/debounce.test.tsx
+++ src/debounce.test.tsx
@@ -1,17 +1,6 @@
-import { act, render, waitFor } from "@testing-library/react";
+import { act, renderHook, waitFor } from "@testing-library/react";
 import { vi, beforeEach, afterEach, expect, it } from "vitest";
 import { useDebounce } from "./debounce.js";
-
-interface Props {
-  value: number;
-  onChange: () => void;
-}
-
-const Component = ({ value, onChange }: Props) => {
-  useDebounce(onChange, 1, [value]);
-
-  return null;
-};
 
 beforeEach(() => {
   vi.useFakeTimers();
@@ -22,74 +11,72 @@ afterEach(() => {
 });
 
 it("runs without any error", () => {
-  render(<Component onChange={() => {}} value={1} />);
+  renderHook(() => useDebounce(() => {}, 1, []));
 });
 
 it("runs a callback", async () => {
   let index = 0;
 
-  await act(() => render(<Component onChange={() => index++} value={1} />));
+  act(() => {
+    renderHook(() => useDebounce(() => index++, 1, []));
+  });
 
   await waitFor(() => expect(index).toBe(1));
 });
 
 it("runs a callback twice", async () => {
+  let value = 0;
   let index = 0;
 
-  const renderComponent = (value: number) =>
-    render(
-      <Component
-        onChange={() => {
-          index++;
-        }}
-        value={value}
-      />,
-    );
+  const { rerender } = renderHook(() => useDebounce(() => index++, 1, [value]));
 
-  await act(() => renderComponent(1));
+  value++;
+  rerender();
 
   await waitFor(() => expect(index).toBe(1));
 
-  await act(() => renderComponent(2));
+  value++;
+  rerender();
 
   await waitFor(() => expect(index).toBe(2));
 });
 
 it("debounces two calls into one", async () => {
+  let value = 0;
   let index = 0;
 
-  const { rerender } = await act(() =>
-    render(<Component onChange={() => index++} value={0} />),
-  );
+  const { rerender } = renderHook(() => useDebounce(() => index++, 1, [value]));
 
-  const renderComponent = (value: number) =>
-    act(() => rerender(<Component onChange={() => index++} value={value} />));
-
-  renderComponent(1);
+  value++;
+  rerender();
+  value++;
+  rerender();
 
   await waitFor(() => expect(index).toBe(1));
 
-  renderComponent(2);
+  value++;
+  rerender();
 
   await waitFor(() => expect(index).toBe(2));
 });
 
 it("debounces three calls into one", async () => {
+  let value = 0;
   let index = 0;
 
-  const { rerender } = await act(() =>
-    render(<Component onChange={() => index++} value={0} />),
-  );
+  const { rerender } = renderHook(() => useDebounce(() => index++, 1, [value]));
 
-  const renderComponent = (value: number) =>
-    rerender(<Component onChange={() => index++} value={value} />);
-
-  renderComponent(1);
-  renderComponent(2);
+  value++;
+  rerender();
+  value++;
+  rerender();
+  value++;
+  rerender();
 
   await waitFor(() => expect(index).toBe(1));
 
-  renderComponent(3);
+  value++;
+  rerender();
 
   await waitFor(() => expect(index).toBe(2));
 });
