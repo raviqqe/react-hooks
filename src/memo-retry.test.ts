6bd821712a511c5588022613b7e7239dd21b30ff
Yota Toyama <raviqqe@gmail.com>
2024-08-08 19:41:48 +0900

    Fix

--- src/memo-retry.test.ts
+++ src/memo-retry.test.ts
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react";
 import { expect, it } from "vitest";
 import { useMemoRetry } from "./memo-retry.js";
 
@@ -35,4 +35,16 @@ it("updates a value", () => {
   rerender();
 
   expect(result.current[0]).toBe(2);
+});
+
+it("retries a value computation", () => {
+  let index = 0;
+
+  const { result } = renderHook(() => useMemoRetry(() => index++, []));
+
+  expect(result.current[0]).toBe(0);
+
+  act(() => result.current[1]());
+
+  expect(result.current[0]).toBe(1);
 });
