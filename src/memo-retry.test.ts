a6deaa97eefafc4a8aefa9e3f1d35a967995634a
Yota Toyama <raviqqe@gmail.com>
2024-08-08 19:40:54 +0900

    fix

--- src/memo-retry.test.ts
+++ src/memo-retry.test.ts
@@ -1,4 +1,4 @@
-import { renderHook, act } from "@testing-library/react";
+import { renderHook } from "@testing-library/react";
 import { expect, it } from "vitest";
 import { useMemoRetry } from "./memo-retry.js";
 
