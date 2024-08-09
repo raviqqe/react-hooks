95eb707e4f1d10324466d8de2352cf07ecb77025
Yota Toyama <raviqqe@gmail.com>
2024-08-09 22:34:29 +0900

    Fix

--- src/async.ts
+++ src/async.ts
@@ -24,6 +24,7 @@ export const useAsync = <T>(
     }
 
     const previousId = ++id.current;
+
     startTransition(() => {
       if (previousId === id.current) {
         setState({ loading: true });
