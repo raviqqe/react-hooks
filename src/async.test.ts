61f1d7babab8176624ab931f04d58d6485e7a1c3
Yota Toyama <raviqqe@gmail.com>
2024-08-09 23:03:02 +0900

    Fix

--- src/async.test.ts
+++ src/async.test.ts
@@ -1,7 +1,6 @@
 import { renderHook, waitFor } from "@testing-library/react";
 import { expect, it } from "vitest";
 import { useAsync } from "./async.js";
-import { sleep } from "@raviqqe/loscore/async";
 
 it("runs without any error", () => {
   renderHook(() => useAsync(async () => null, []));
@@ -13,8 +12,6 @@ it("runs a callback", async () => {
   const { result, rerender } = renderHook(() =>
     useAsync(async () => index++, []),
   );
-
-  await sleep(1);
 
   await waitFor(() => {
     rerender();
