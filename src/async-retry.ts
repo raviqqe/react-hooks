e2814e91ce17cb5eee6ac6b177463f1712c290ac
Yota Toyama <raviqqe@gmail.com>
2024-08-10 20:42:09 +0900

    Fix

--- src/async-retry.ts
+++ src/async-retry.ts
@@ -2,10 +2,14 @@ import { type DependencyList } from "react";
 import { useToggle } from "./toggle.js";
 import { type AsyncState, useAsync } from "./async.js";
 
+export interface AsyncRetryState<T> extends AsyncState<T> {
+  retry: () => void;
+}
+
 export const useAsyncRetry = <T>(
   callback: () => Promise<T>,
   dependencies?: DependencyList,
-): AsyncState<T> & { retry: () => void } => {
+): AsyncRetryState<T> => {
   const [on, retry] = useToggle(false);
 
   const state = useAsync(callback, [on, ...(dependencies ?? [])]);
