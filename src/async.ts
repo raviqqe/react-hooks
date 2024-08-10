a3ecbdb74e302a1ee96b6bbb532eda5f06919d8b
Yota Toyama <raviqqe@gmail.com>
2024-08-10 20:37:26 +0900

    Fix

--- src/async.ts
+++ src/async.ts
@@ -7,35 +7,41 @@ import {
 } from "react";
 
 export type AsyncState<T> =
-  | { loading: false; value: T }
-  | { loading: false; error: unknown }
-  | { loading: true };
+  | { error: undefined; loading: false; value: T }
+  | { error: unknown; loading: false; value: undefined }
+  | { error: undefined; loading: true; value: undefined };
+
+const loadingState: AsyncState<never> = {
+  error: undefined,
+  loading: true,
+  value: undefined,
+};
 
 export const useAsync = <T>(
   callback: () => Promise<T>,
   dependencies?: DependencyList,
 ): AsyncState<T> => {
   const id = useRef(0);
-  const [state, setState] = useState<AsyncState<T>>({ loading: true });
+  const [state, setState] = useState<AsyncState<T>>(loadingState);
 
   useEffect(() => {
     const previousId = ++id.current;
 
     startTransition(() => {
       if (previousId === id.current) {
-        setState({ loading: true });
+        setState(loadingState);
       }
     });
 
     void callback()
       .then((value) => {
         if (previousId === id.current) {
-          setState({ loading: false, value });
+          setState({ error: undefined, loading: false, value });
         }
       })
       .catch((error) => {
         if (previousId === id.current) {
-          setState({ error, loading: false });
+          setState({ error, loading: false, value: undefined });
         }
       });
     // eslint-disable-next-line react-hooks/exhaustive-deps
