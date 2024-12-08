8e7692328560395cc3c2710f902bbfddb46675d5
Yota Toyama <raviqqe@gmail.com>
2024-12-08 09:53:34 +0900

    Fix

--- src/infinite-scroll.ts
+++ src/infinite-scroll.ts
@@ -2,10 +2,10 @@ import { useAsync } from "./async.js";
 import { type Ref } from "react";
 import { useVisible } from "./visible.js";
 
-export const useInfiniteScroll = (
+export const useInfiniteScroll = <T extends HTMLElement>(
   callback: () => Promise<void> | void,
-): Ref<HTMLDivElement> => {
-  const { ref, visible } = useVisible();
+): Ref<T> => {
+  const { ref, visible } = useVisible<T>();
 
   useAsync(async () => {
     if (visible) {
