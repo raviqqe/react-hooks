73b7b6ec2ca2ba0f733260bd17000cc92a37bb21
Yota Toyama <raviqqe@gmail.com>
2024-12-08 10:10:55 +0900

    Use rslib (#306)

---
+++ rslib.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "@rslib/core";
+
+export default defineConfig({
+  lib: [
+    {
+      dts: true,
+      format: "esm",
+      output: {
+        minify: true,
+      },
+    },
+  ],
+  source: {
+    entry: {
+      index: "./src/index.ts",
+    },
+  },
+});
