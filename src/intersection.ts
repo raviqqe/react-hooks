693909230b0961b0a100bf43d9be61cb54ba772e
Yota Toyama <raviqqe@gmail.com>
2024-12-27 14:32:39 +0900

    Fix

--- src/intersection.ts
+++ src/intersection.ts
@@ -1,4 +1,4 @@
-import { RefObject, useEffect, useState } from "react";
+import { type RefObject, useEffect, useState } from "react";
 
 export const useIntersection = <T extends HTMLElement>(
   ref: RefObject<T | null>,
