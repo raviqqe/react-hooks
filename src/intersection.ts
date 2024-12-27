2d508e1cbd68eb33ae9daf538527cc645be3b4ca
Yota Toyama <raviqqe@gmail.com>
2024-12-27 14:29:37 +0900

    Fix

--- src/intersection.ts
+++ src/intersection.ts
@@ -6,17 +6,19 @@ export const useIntersection = (
   const [entry, setEntry] = useState<IntersectionObserverEntry | null>(null);
 
   useEffect(() => {
-    if (ref.current) {
-      const observer = new IntersectionObserver(([entry]) =>
-        setEntry(entry ?? null),
-      );
-      observer.observe(ref.current);
-
-      return () => {
-        setEntry(null);
-        observer.disconnect();
-      };
+    if (!ref.current) {
+      return;
     }
+
+    const observer = new IntersectionObserver(([entry]) =>
+      setEntry(entry ?? null),
+    );
+    observer.observe(ref.current);
+
+    return () => {
+      setEntry(null);
+      observer.disconnect();
+    };
   }, [ref.current]);
 
   return entry;
