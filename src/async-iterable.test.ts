29876f7c34ff584789350e3ba6943ef666f9a614
Yota Toyama <raviqqe@gmail.com>
2025-05-04 13:51:31 -0700

    Fix

--- src/async-iterable.test.ts
+++ src/async-iterable.test.ts
@@ -3,13 +3,10 @@ import { expect, it } from "vitest";
 import { useAutomaticAsyncIterable } from "./async-iterable.js";
 
 it("runs without any error", async () => {
-  const { result } = renderHook(() =>
-    useAutomaticAsyncIterable(
-      (async function* () {
-        yield 42;
-      })(),
-    ),
-  );
+  const iterator = (async function* () {
+    yield 42;
+  })();
+  const { result } = renderHook(() => useAutomaticAsyncIterable(iterator));
 
   await waitFor(async () => expect(result.current.value).toEqual([42]));
 });
