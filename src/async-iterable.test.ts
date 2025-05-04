d1b4c28c9e2c1d54afc81f3fc30a2d174eb7593e
Yota Toyama <raviqqe@gmail.com>
2025-05-04 14:39:46 -0700

    Fix

--- src/async-iterable.test.ts
+++ src/async-iterable.test.ts
@@ -1,7 +1,6 @@
 import { renderHook, waitFor } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import { useAutomaticAsyncIterable } from "./async-iterable.js";
-import { delay } from "es-toolkit";
 
 describe(useAutomaticAsyncIterable.name, () => {
   it("collects no value", async () => {
