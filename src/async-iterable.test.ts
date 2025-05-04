68f097bccef5fd191a2bfca9aa99faa38c026323
Yota Toyama <raviqqe@gmail.com>
2025-05-04 12:23:33 -0700

    Fix

---
+++ src/async-iterable.test.ts
@@ -0,0 +1,9 @@
+import { renderHook } from "@testing-library/react";
+import { it } from "vitest";
+import { useAsyncIterable } from "./async-iterable.js";
+
+it("runs without any error", () => {
+  renderHook(() => useAsyncIterable((async function*() {
+    yield 42;
+  })()));
+});
