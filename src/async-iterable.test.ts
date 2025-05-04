bf62cfe60e6dda595599e218c7d5c0b66837c04b
Yota Toyama <raviqqe@gmail.com>
2025-05-04 12:25:16 -0700

    Fix

--- src/async-iterable.test.ts
+++ src/async-iterable.test.ts
@@ -1,9 +1,11 @@
-import { renderHook } from "@testing-library/react";
-import { it } from "vitest";
+import { renderHook, waitFor } from "@testing-library/react";
+import { expect, it } from "vitest";
 import { useAsyncIterable } from "./async-iterable.js";
 
-it("runs without any error", () => {
-  renderHook(() => useAsyncIterable((async function*() {
+it("runs without any error", async () => {
+  const { result } = renderHook(() => useAsyncIterable((async function*() {
     yield 42;
   })()));
+
+  await waitFor(() => expect(result.current.value).toEqual([42]));
 });
